09f0e4c2a529671bfbd6e5521d0e09b20d209efa
Shal1n1 <89414838+Shal1n1@users.noreply.github.com>
2021-08-24 14:12:06 +0530

    Js Day1

---
+++ Js Day1/script.js
@@ -0,0 +1,13 @@
+a = 74
+b = 56
+sum = a+b
+diff = a-b
+mul = a*b
+div = a/b
+
+console.log("The sum is", sum)
+console.log("The difference is", diff)
+console.log("The multiplication is", mul)
+console.log("the division is", div)
+console.log("The multiplication of",a,'*',b,"is",mul)
+console.log('Division of s{a}/s{b} is s{div}')
